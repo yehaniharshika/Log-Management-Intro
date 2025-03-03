6f73ef040495cf7427c10dc9e5a3f84bc016b265
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-03-03 10:29:48 +0530

    first commit

---
+++ src/main/java/lk/ijse/logmanagement/LogManagementApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.logmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogManagementApplication.class, args);
+    }
+
+}
